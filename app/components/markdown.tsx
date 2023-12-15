2384f69c1371cef0368c3a6e9a3a45f442f17b9a
tianlu17 <tianlu17@xdf.cn>
2023-12-15 14:21:31 +0800

    chore: readme

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -86,6 +86,7 @@ export function PreCode(props: { children: any }) {
 }
 
 function _MarkDownContent(props: { content: string }) {
+  console.log(333, props.content);
   return (
     <ReactMarkdown
       remarkPlugins={[RemarkMath, RemarkGfm, RemarkBreaks]}
@@ -154,6 +155,8 @@ export function Markdown(
   };
 
   setTimeout(() => checkInView(), 1);
+
+  console.log(88888, props.content);
 
   return (
     <div
